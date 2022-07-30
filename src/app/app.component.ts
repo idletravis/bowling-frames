3c0036f9f8745a2315e73ffe14a2994ee0c7f297
idletravis <idle.travis@yahoo.com>
2022-07-30 05:29:10 -0500

    Added Score column after 10th frame.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Bowling-JS';
   frameNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  columnNames: string[] = ['bowler', 'frame1', 'frame2', 'frame3', 'frame4', 'frame5', 'frame6', 'frame7', 'frame8', 'frame9', 'frame10',];
-  bowlers: string[] = ['Travis'];
+  columnNames: string[] = ['bowler', 'frame1', 'frame2', 'frame3', 'frame4', 'frame5', 'frame6', 'frame7', 'frame8', 'frame9', 'frame10', 'score'];
+  bowlers: Bowler[] = [{name: 'Travis', score: 50}];
+}
+
+interface Bowler {
+  name: string,
+  score?: number
 }
