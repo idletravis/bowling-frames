657d8010dd822adfeb45b869687a4c936f4b8a5f
jokersupreme <caraza.jose598@gmail.com>
2022-12-07 18:46:01 -0600

    Created Bowler class

--- src/app/domain/Frame.ts
+++ src/app/domain/Frame.ts
@@ -16,17 +16,7 @@ export class Frame {
     public generatePinDropped(): void {
         this.throws[this.isFirstThrow ? 0 : 1] = Math.floor(Math.random() * (11 - this.throws[0]));
         console.log(this.isFirstThrow ? 'First' : 'Second', 'Throw: ', this.throws[this.isFirstThrow ? 0 : 1]);
-
-        if (!this.isFirstThrow) {
-            this.sumScore = this.throws[0] + this.throws[1];
-        } else if (this.previousFrame?.isAStrike() && !this.isFirstThrow) {
-            this.previousFrame.calculateStrike(this.throws[0], this.throws[1]);
-        } else if (this.previousFrame?.hasASpare && this.isFirstThrow) {
-            this.previousFrame.calculateSpare(this.throws[0]);
-        }
-
-        this.isFirstThrow = false;
-        this.hasASpare = this.isASpare();
+        
     }
 
     public calculateSpare(thisSpare: number): void {
