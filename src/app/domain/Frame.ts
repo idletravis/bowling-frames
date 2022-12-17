51e527a3c780e4754e68d329d198f3ce2538a592
jokersupreme <caraza.jose598@gmail.com>
2022-12-16 21:12:35 -0600

    Improved logic for knocked down pins and added ability to determine if frame is done. Also set up bowler class.

--- src/app/domain/Frame.ts
+++ src/app/domain/Frame.ts
@@ -1,22 +1,31 @@
 
 
 export class Frame {
-
     maxPins: number = 10;
     sumScore: number = 0;
-    throws: number[] = [0, 0];
-    isFirstThrow: boolean = true;
+    throws: number[] = [];
+    frameDone: boolean = false;
     hasASpare: boolean = false;
     previousFrame!: Frame | undefined;
+    nextFrame: Frame;
 
-    constructor(thisFrame: Frame | undefined) {
-        this.previousFrame = thisFrame;
+    constructor(previous: Frame | undefined, next: Frame) {
+        this.previousFrame = previous;
+        this.nextFrame = next;
+    }
+
+    // throw
+    // 1. generatePinDropped
+    // 2. determines frameDone
+    public throw(): void{
+        this.generatePinDropped();
+        this.determineFrameDone();
     }
 
     public generatePinDropped(): void {
-        this.throws[this.isFirstThrow ? 0 : 1] = Math.floor(Math.random() * (11 - this.throws[0]));
-        console.log(this.isFirstThrow ? 'First' : 'Second', 'Throw: ', this.throws[this.isFirstThrow ? 0 : 1]);
-
+        const thisThrow = Math.floor(Math.random() * (11 - this.throws[0]));
+        this.throws.push(thisThrow);
+        console.log(`This throw: ${thisThrow}`);
     }
 
     public calculateScore(firstThrow: number, secondThrow: number){
@@ -30,11 +39,21 @@ export class Frame {
         this.sumScore = 10 + (firstThrow + secondThrow);
     }
 
+    public determineFrameDone() {
+        if (this.throws.length == 2 || this.isAStrike()) {
+            this.frameDone = true;
+        }
+    }
+
     public isASpare(): boolean {
         return (this.throws[0] + this.throws[1]) === 10 && this.throws[0] != 10;
     }
 
     public isAStrike(): boolean {
         return this.throws[0] == 10;
+    }
+
+    public isDone(): boolean {
+        return this.frameDone;
     }
 }
